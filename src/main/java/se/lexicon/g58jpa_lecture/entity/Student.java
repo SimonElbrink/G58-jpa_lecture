f86a514398150ffa40e370045e85f917095ef083
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2026-01-21 09:58:58 +0100

    Fetch type
     FetchType: What is FetchType and When to Use It?
        FetchType is an enumeration in JPA that defines how related entities should be fetched from the database.
        It determines the strategy for loading related entities when the parent entity is loaded.
        There are two main fetch types:
        1. EAGER: This fetch type means that the related entity will be loaded immediately when the parent entity is loaded.
           It is useful when you know that you will always need the related entity along with the parent entity.
           However, it can lead to performance issues if there are many related entities or if the relationship is deep.
        2. LAZY: This fetch type means that the related entity will be loaded only when it is explicitly accessed.
             It is useful when you want to avoid loading unnecessary data and improve performance.
             it loads the related entity only when getAddress() is called.
    
             However, it can lead to issues if the session is closed before accessing the related entity, resulting in a LazyInitializationException.

--- src/main/java/se/lexicon/g58jpa_lecture/entity/Student.java
+++ src/main/java/se/lexicon/g58jpa_lecture/entity/Student.java
@@ -37,7 +37,7 @@ public class Student {
     private LocalDateTime createDate;
 
     @Setter
-    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.REFRESH})
+    @OneToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.REFRESH})
     @JoinColumn(name = "address_id", unique = true)
     private Address address;
 
